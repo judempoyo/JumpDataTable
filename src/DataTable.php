c2d87d1a78b7a51332d26bf8d8d90e8026704408
Jude mpoyo <mpoyojude0@gmail.com>
2025-04-21 01:15:47 +0200

    Enhance documentation by adding PHPDoc comments for DataTable and DataTableRenderer methods; improve code clarity and maintainability

--- src/DataTable.php
+++ src/DataTable.php
@@ -4,7 +4,6 @@ namespace Jump\JumpDataTable;
 
 class DataTable
 {
-
     protected string $title = 'Liste des éléments';
     protected string $createUrl = '#';
     protected array $columns = [];
@@ -25,6 +24,14 @@ class DataTable
         'bootstrap' => Themes\BootstrapTheme::class,
     ];
 
+    /**
+     * Set the theme for the DataTable.
+     *
+     * @param string $theme The theme name (e.g., 'tailwind', 'bootstrap').
+     * @param array $customConfig Custom configuration for the theme.
+     * @return self
+     * @throws \InvalidArgumentException If the theme is not supported.
+     */
     public function useTheme(string $theme, array $customConfig = []): self
     {
         if (!array_key_exists($theme, $this->themes)) {
@@ -37,123 +44,223 @@ class DataTable
         return $this;
     }
 
+    /**
+     * Get the default configuration for the current theme.
+     *
+     * @return array The default configuration array.
+     */
     protected function getDefaultConfig(): array
     {
         $themeClass = $this->themes[$this->theme];
         return $themeClass::getDefaultConfig();
     }
 
-
-
+    /**
+     * Create a new instance of the DataTable.
+     *
+     * @return self
+     */
     public static function make(): self
     {
         return new self();
     }
 
-    // Setters with fluent interface
+    /**
+     * Set the title of the DataTable.
+     *
+     * @param string $title The title to display.
+     * @return self
+     */
     public function title(string $title): self
     {
         $this->title = $title;
         return $this;
     }
 
+    /**
+     * Set the URL for the "create" action.
+     *
+     * @param string $url The URL for creating a new item.
+     * @return self
+     */
     public function createUrl(string $url): self
     {
         $this->createUrl = $url;
         return $this;
     }
 
+    /**
+     * Set the columns for the DataTable.
+     *
+     * @param array $columns An array of column configurations.
+     * @return self
+     */
     public function columns(array $columns): self
     {
         $this->columns = $columns;
         return $this;
     }
 
+    /**
+     * Set the data for the DataTable.
+     *
+     * @param iterable|null $data The data to display in the table.
+     * @return self
+     */
     public function data(?iterable $data): self
     {
         $this->data = $data;
         return $this;
     }
 
+    /**
+     * Set the actions for the DataTable.
+     *
+     * @param array $actions An array of action configurations.
+     * @return self
+     */
     public function actions(array $actions): self
     {
         $this->actions = $actions;
         return $this;
     }
 
+    /**
+     * Set the filters for the DataTable.
+     *
+     * @param array $filters An array of filter configurations.
+     * @return self
+     */
     public function filters(array $filters): self
     {
         $this->filters = $filters;
         return $this;
     }
 
+    /**
+     * Set the model name for the DataTable.
+     *
+     * @param string $modelName The name of the model.
+     * @return self
+     */
     public function modelName(string $modelName): self
     {
         $this->modelName = $modelName;
         return $this;
     }
 
+    /**
+     * Enable or disable the export functionality.
+     *
+     * @param bool $showExport Whether to show the export button.
+     * @return self
+     */
     public function showExport(bool $showExport): self
     {
         $this->showExport = $showExport;
         return $this;
     }
 
+    /**
+     * Set the column to sort by.
+     *
+     * @param string $sort The column name to sort by.
+     * @return self
+     */
     public function sort(string $sort): self
     {
         $this->sort = $sort;
         return $this;
     }
 
+    /**
+     * Set the sort direction.
+     *
+     * @param string $direction The sort direction ('asc' or 'desc').
+     * @return self
+     */
     public function direction(string $direction): self
     {
         $this->direction = $direction;
         return $this;
     }
 
+    /**
+     * Set the public URL for the DataTable.
+     *
+     * @param string $publicUrl The public URL.
+     * @return self
+     */
     public function publicUrl(string $publicUrl): self
     {
         $this->publicUrl = $publicUrl;
         return $this;
     }
 
+    /**
+     * Set the pagination configuration.
+     *
+     * @param array $pagination The pagination configuration array.
+     * @return self
+     */
     public function pagination(array $pagination): self
     {
         $this->pagination = $pagination;
         return $this;
     }
 
+    /**
+     * Add a column to the DataTable.
+     *
+     * @param array $column The column configuration.
+     * @return self
+     */
     public function addColumn(array $column): self
     {
         $this->columns[] = $column;
         return $this;
     }
 
+    /**
+     * Add an action to the DataTable.
+     *
+     * @param array $action The action configuration.
+     * @return self
+     */
     public function addAction(array $action): self
     {
         $this->actions[] = $action;
         return $this;
     }
 
+    /**
+     * Add a filter to the DataTable.
+     *
+     * @param array $filter The filter configuration.
+     * @return self
+     */
     public function addFilter(array $filter): self
     {
         $this->filters[] = $filter;
         return $this;
     }
 
-    /*  public function render(): string
-     {
-         $renderer = new DataTableRenderer();
-         return $renderer->render($this->toArray());
-     }
-  */
+    /**
+     * Render the DataTable.
+     *
+     * @return string The rendered HTML for the DataTable.
+     */
     public function render(): string
     {
         $renderer = new DataTableRenderer($this->theme);
         return $renderer->render($this->toArray());
     }
 
-
+    /**
+     * Convert the DataTable configuration to an array.
+     *
+     * @return array The DataTable configuration as an array.
+     */
     public function toArray(): array
     {
         return [

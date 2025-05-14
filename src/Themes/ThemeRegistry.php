9bcb64c13d3907086b4ecb3bcfde9b946c39ea25
Jude mpoyo <mpoyojude0@gmail.com>
2025-05-14 16:21:27 +0200

    `Added Symfony Finder component and improved ThemeRegistry class`

--- src/Themes/ThemeRegistry.php
+++ src/Themes/ThemeRegistry.php
@@ -2,12 +2,13 @@
 
 namespace Jump\JumpDataTable\Themes;
 
+use Symfony\Component\Finder\Finder;
+
 class ThemeRegistry
 {
     private static array $themes = [
         'tailwind' => "Jump\\JumpDataTable\\Themes\\TailwindTheme",
         'bootstrap' => "Jump\\JumpDataTable\\Themes\\BootstrapTheme"
-        
     ];
 
     public static function register(string $name, string $class, ?string $filePath = null): void
@@ -23,13 +24,35 @@ class ThemeRegistry
         self::$themes[$name] = $class;
     }
 
+    public static function discover(string $directory, string $namespacePrefix): void
+    {
+        $finder = new Finder();
+        $finder->files()
+            ->in($directory)
+            ->name('*Theme.php')
+            ->sortByName();
+
+        foreach ($finder as $file) {
+            $className = $namespacePrefix.'\\'.$file->getBasename('.php');
+            if (class_exists($className) && in_array(ThemeInterface::class, class_implements($className))) {
+                $themeName = strtolower(str_replace('Theme', '', $file->getBasename('.php')));
+                self::register($themeName, $className);
+            }
+        }
+    }
+
     public static function get(string $name): string
     {
         if (!isset(self::$themes[$name])) {
+            $available = array_map(
+                fn($k) => "<comment>$k</comment>", 
+                array_keys(self::$themes)
+            );
+            
             throw new \InvalidArgumentException(sprintf(
-                "Theme '%s' not registered. Available: %s",
+                "Theme '%s' not registered. Available themes:\n%s",
                 $name,
-                implode(', ', array_keys(self::$themes))
+                implode(', ', $available)
             ));
         }
         
@@ -39,5 +62,10 @@ class ThemeRegistry
     public static function all(): array
     {
         return self::$themes;
+    }
+
+    public static function isRegistered(string $name): bool
+    {
+        return isset(self::$themes[$name]);
     }
 }
